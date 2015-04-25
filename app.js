54926542ab09a178bcfec1952d4f229956370a4b
Francisco De La Cruz <helloquico@gmail.com>
2015-04-24 23:27:27 -0400

    The Constructor Pattern

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
-var johnDoe = {
-	firstName: "John",
-	lastName: "Doe",
-	sayName: function() {
-		return "My name is " + this.firstName + " " + this.lastName + ".";
-	}
+function Person(firstName, lastName) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+
+	// Increased Memory usage since every object has its own method
+	// this.sayName = function() {
+	// 	return "My name is " + this.firstName + " " + this.lastName;
+	// };
+}
+
+// Better, only own one shared with every object
+Person.prototype.sayName = function() {
+	return "My name is " + this.firstName + " " + this.lastName;
 };
 
-var janeDoe = Object.create(johnDoe, {
-	firstName: {
-		value: "Jane"
-	},
-	greet: {
-		value: function(person) {
-			return "Hello, " + person.firstName;
-		}
-	}
-});
+var johnDoe = new Person("John", "Doe");
+var janeDoe = new Person("Jane", "Doe");
 
-var jimSmith = Object.create(janeDoe, {
-	firstName: {
-		value: "Jim"
-	},
-	lastName: {
-		value: "Smith"
-	}
-});
-
-console.log(johnDoe.sayName());
-console.log(janeDoe.sayName() + " " + janeDoe.greet(johnDoe));
-console.log(jimSmith.sayName() + " " + jimSmith.greet(janeDoe));
-
-console.log(janeDoe.__proto__ === johnDoe);
-console.log(jimSmith.__proto__ === janeDoe);
+var isPerson = johnDoe instanceof Person; //true
+var isSame = johnDoe.sayName === janeDoe.sayName; //true
