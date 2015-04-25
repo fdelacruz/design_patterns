0dae5deadebdb0bb604adcfcfdb745e057f89604
Francisco De La Cruz <helloquico@gmail.com>
2015-04-24 23:26:49 -0400

    Generate Objects with Object.create()

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-var obj = {
-	firstName: "Francisco",
-	lastName: "De La Cruz"
+var johnDoe = {
+	firstName: "John",
+	lastName: "Doe",
+	sayName: function() {
+		return "My name is " + this.firstName + " " + this.lastName + ".";
+	}
 };
 
-obj.firstName = "Francisco";
-obj["lastName"] = "De La Cruz";
-
-var firstName = obj["lastName"];
-var lastName = obj.lastName;
-
-Object.defineProperty(obj, "country", {
-	value: "USA"
-});
-
-Object.defineProperties(obj, {
-	twitter: {
-		value: "fdelacruz"
+var janeDoe = Object.create(johnDoe, {
+	firstName: {
+		value: "Jane"
 	},
-	email: {
-		value: "helloquico@gmail.com"
+	greet: {
+		value: function(person) {
+			return "Hello, " + person.firstName;
+		}
 	}
 });
+
+var jimSmith = Object.create(janeDoe, {
+	firstName: {
+		value: "Jim"
+	},
+	lastName: {
+		value: "Smith"
+	}
+});
+
+console.log(johnDoe.sayName());
+console.log(janeDoe.sayName() + " " + janeDoe.greet(johnDoe));
+console.log(jimSmith.sayName() + " " + jimSmith.greet(janeDoe));
+
+console.log(janeDoe.__proto__ === johnDoe);
+console.log(jimSmith.__proto__ === janeDoe);
