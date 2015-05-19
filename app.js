fad12d90ac7c8f132925f2174b56c9cb6de7e7d4
Francisco De La Cruz <helloquico@gmail.com>
2015-05-19 18:06:37 -0400

    The Facade Pattern

--- app.js
+++ app.js
@@ -1,52 +1,19 @@
-function Calculator() {
-	this._currentValue = 0;
-	this.commands = [];
-}
-
-Calculator.prototype = {
-	execute: function(command) {
-		this._currentValue = command.execute(this._currentValue);
-		this.commands.push(command);
-	},
-	undo: function() {
-		var cmd = this.commands.pop();
-		
-		this._currentValue = cmd.undo(this._currentValue);
-	},
-	getCurrentValue: function() {
-		return this._currentValue;
+function addEvent(el, ev, fn) {
+	if (el.addEventListener) {
+		el.addEventListener(ev, fn, false);
+	} else if (el.attachEvent){
+		el.attachEvent('on' + ev, fn);
+	} else {
+		el['on' + ev] = fn;
 	}
-};
-
-function add(value) {
-	return value + this.value;
 }
 
-function sub(value) {
-	return value - this.value;
+function removeEvent(el, ev, fn) {
+	if (el.removeEventListener) {
+		el.removeEventListener(ev, fn, false);
+	} else if (el.detachEvent){
+		el.detachEvent('on' + ev, fn);
+	} else {
+		el['on' + ev] = null;
+	}
 }
-
-// Command Abstract Object Constructor
-function Command(fn, undo, value) {
-	this.execute = fn;
-	this.value = value;
-	this.undo = undo;
-}
-
-// Command 'operator' Constructors using our Command Abstract Object Constructor
-function AddCommand(value) {
-	Command.call(this, add, sub, value);
-}
-
-function SubCommand(value) {
-	Command.call(this, sub, add, value);
-}
-
-var calc = new Calculator();
-
-// calc.execute(new AddCommand(19))
-// calc.getCurrentValue() -> 19
-// calc.execute(new AddCommand(1))
-// calc.getCurrentValue() -> 20
-// calc.undo()
-// calc.getCurrentValue() -> 19
